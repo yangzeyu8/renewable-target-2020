5220f507a9a976a888a9629f0e7038c1894ad3bc
Zeyu Yang <45043057+yangzeyu8@users.noreply.github.com>
2020-12-25 06:57:58 +0800

    Update OCM.m

--- OCM.m
+++ OCM.m
@@ -1,11 +1,8 @@
 % Solver-based quadratic programming for Operating Cost Minimization (OCM)
-% This function returns struct var and struct cost
-% var has 11 attributes
-% cost has 4 attributes
-% r_S and r_W are 24*1, gamma_pac is 24*24
-% yzy 2020/12/21
+% Shape of param.r_S and param.r_W is 24*10
+% yzy, Glasgow College, UESTC
 
-function [var, cost] = OCM(param, Tin_ref, Tout, Tin0, Pf_ref, Pc_ref, UBpre, Pil, theta, Eb0, D, M)
+function [var, cost] = OCM(param, Tin_ref, Tout, Tin0, Pf_ref, Pc_ref, Pil, theta, Eb0, D, M)
 
 %% User Model Variable Index
 idx = [];
@@ -90,9 +87,8 @@ LBpc = param.LBpc;
 
 % D. Renewable Power Supply Input Parameters
 
-% 7. UBpre % upper boundary of renewable energy 24*1
-% 8. Pil % inflexible load
-% 9. theta % renewable target percentage
+% 7. Pil % inflexible load
+% 8. theta % renewable target percentage
 
 % E. Grid Power Supply Input Parameters
 
@@ -103,8 +99,8 @@ LBpg = param.LBpg;
 
 % G. Battery Input Parameters
 
-% 10. Eb0 % initial battery energy
-% 11. D % how many days do we observe
+% 9. Eb0 % initial battery energy
+% 10. D % how many days do we observe
 
 UBpch = param.UBpch;
 LBpch = param.LBpch;
@@ -117,22 +113,22 @@ LBeb = param.LBeb;
 
 % H. Solar Energy Investment Input Parameters
 
-r_S = param.r_S; % power per unit of invested soalr capacity
+r_S = param.r_S(:,1); % power per unit of invested soalr capacity
 c_S = param.c_S; % investment cost of solar power per kW
 
 % I. Wind Energy Investment Input Parameters
 
-r_W = param.r_W; % power per unit of invested wind capacity
+r_W = param.r_W(:,1); % power per unit of invested wind capacity
 c_W = param.c_W; % investment cost of wind power per kW
 
 % J. Battery Energy Investment Input Parameters
 
-L_B = param.L_B; % life span energy per unit of invested battery capacity
+L_B = param.L_B; % life span energy per unit of invested battery capacity
 c_B = param.c_B; % investment cost of battery power per kW
 
 % K. Operator Cost
 
-% 12. M % the investment budget
+% 11. M % the investment budget
 
 gamma_po = param.gamma_po;
 
@@ -143,9 +139,7 @@ C = 3.3;  R = 1.35;  eta_pac = 7;
 a = (1 - 1/(C*R));  b = 1/(C*R);  c = eta_pac/C;
 
 H_pac = zeros(idx_len, idx_len);
-
-% Here gamma_pac is 24*24
-H_pac(idx_tin,idx_tin) = 2*gamma_pac; % param.gamma_pac
+H_pac(idx_tin,idx_tin) = 2*gamma_pac*eye(24); % param.gamma_pac
 
 f_pac = zeros(idx_len, 1);
 f_pac(idx_tin,1) = -2*gamma_pac*Tin_ref*ones(24,1); % Tin_ref
@@ -212,6 +206,15 @@ f_po = zeros(idx_len,1);
 f_po(idx_z,1) = pi1*ones(24,1);
 f_po(idx_zmax,1) = pi2;
 
+% Constraint: calculation of pa
+beq_pa = zeros(24,1);
+
+Aeq_pa = zeros(24, idx_len);
+Aeq_pa(:,idx_pa) = eye(24);
+Aeq_pa(:,idx_pre) = -eye(24);
+Aeq_pa(:,idx_pg) = -eye(24);
+
+
 % Constraint: calculation of po
 beq_po = zeros(24,1);
 
@@ -255,6 +258,9 @@ lb_pg = ones(24,1)*LBpg; % param.LBpg
 ub_pg = ones(24,1)*UBpg; % param.UBpg
 
 % Constraint: calculation of battery storage eb
+eta_ch = 0.9;
+eta_dis = 0.9;
+
 beq_eb = zeros(24,1);
 
 Aeq_eb = zeros(24, idx_len);
@@ -301,11 +307,18 @@ b_pdis = zeros(1,1);
 
 A_pdis = zeros(1, idx_len);
 A_pdis(:,idx_pdis) = D*ones(24,1); % D, the number of days we observe
-A_pdis(:,idx_beta_B) = -L_B;
+A_pdis(:,idx_beta_B) = -L_B;
 
 % Constraint: boundaries of pre
 lb_pre = zeros(24,1);
-ub_pre = UBpre; % UBpre
+% ub_pre = UBpre; % UBpre
+
+b_pre_beta = zeros(24,1);
+
+A_pre_beta = zeros(24, idx_len);
+A_pre_beta(:, idx_pre) = eye(24);
+A_pre_beta(:, idx_beta_S) = -r_S;
+A_pre_beta(:, idx_beta_W) = -r_W;
 
 % Constraint: calculation of pre
 beq_pre = Pil; % Pil, the inflexible load
@@ -316,14 +329,15 @@ Aeq_pre(:,idx_pg) = eye(24);
 Aeq_pre(:,idx_pdis) = eye(24);
 Aeq_pre(:,idx_pac) = -eye(24);
 Aeq_pre(:,idx_pf) = -eye(24);
+Aeq_pre(:,idx_pc) = -eye(24);
 Aeq_pre(:,idx_pch) = -eye(24);
 
 % Constraint: renewable target pre
-b_pre = zeros(24,1);
+b_pre = zeros(1,1);
 
 A_pre = zeros(1, idx_len);
-A_pre(:,idx_pre) = ones(24,1);
-A_pre(:,idx_pa) = -theta*ones(24,1);
+A_pre(:,idx_pre) = -ones(1,24);
+A_pre(:,idx_pa) = theta*ones(1,24);
 
 % Constraint: bugdet limit
 b_beta = M; % M, the total investment budget
@@ -332,11 +346,6 @@ A_beta = zeros(1, idx_len);
 A_beta(:,idx_beta_S) = c_S*ones(1,1);
 A_beta(:,idx_beta_W) = c_W*ones(1,1);
 A_beta(:,idx_beta_B) = c_B*ones(1,1);
-
-% Constraint: boundaries of beta_S, beta_W, beta_B
-lb_beta_S = zeros(1,1);
-lb_beta_W = zeros(1,1);
-lb_beta_B = zeros(1,1);
 
 %% Calculation of Investment of Renewable Energy 
 
@@ -354,15 +363,11 @@ f_beta_B(idx_beta_B, 1) = c_B;
 
 %% Optimization Setup
 
-H = D*(H_pac + H_pc + H_pf);
+Aeq = [Aeq_tin0; Aeq_pac; Aeq_pf; Aeq_pre; Aeq_eb; Aeq_eb0; Aeq_pa; Aeq_po];
+beq = [beq_tin0; beq_pac; beq_pf; beq_pre; beq_eb; beq_eb0; beq_pa; beq_po]; 
 
-f = D*(f_pac + f_pc + f_pf + f_po) + f_beta_S + f_beta_W + f_beta_B;
-
-Aeq = [Aeq_tin0; Aeq_pac; Aeq_pf; Aeq_pre; Aeq_eb; Aeq_eb0; Aeq_po];
-beq = [beq_tin0; beq_pac; beq_pf; beq_pre; beq_eb; beq_eb0; beq_po]; 
-
-A = [A_beta; A_eb; A_pch; A_pdis_beta; A_pdis; A_po; A_z; A_zmax; A_pre];
-b = [b_beta; b_eb; b_pch; b_pdis_beta; b_pdis; b_po; b_z; b_zmax; b_pre];
+A = [A_beta; A_eb; A_pch; A_pdis_beta; A_pdis; A_po; A_z; A_zmax; A_pre_beta; A_pre];
+b = [b_beta; b_eb; b_pch; b_pdis_beta; b_pdis; b_po; b_z; b_zmax; b_pre_beta; b_pre];
 
 ub = ones(idx_len,1) * 500000;
 ub(idx_pac, 1) = ub_pac;
@@ -370,7 +375,6 @@ ub(idx_tin, 1) = ub_tin;
 ub(idx_pf, 1) = ub_pf;
 ub(idx_pc, 1) = ub_pc;
 ub(idx_pg, 1) = ub_pg;
-ub(idx_pre, 1) = ub_pre;
 
 lb = zeros(idx_len, 1);
 lb(idx_pac, 1) = lb_pac;
@@ -385,10 +389,14 @@ lb(idx_pre, 1) = lb_pre;
 lb(idx_z, 1) = lb_z;
 lb(idx_zmax, 1) = lb_zmax;
 
+H = D*(H_pac + H_pc + H_pf);
+
+f = D*(f_pac + f_pc + f_pf + f_po) + f_beta_S + f_beta_W + f_beta_B;
+
 options = optimoptions('quadprog','Display','off');
-[p_user, fval, exitflag, output] = quadprog(H,f,A,b,Aeq,beq,lb,ub,[],options);
+[p_user, ~, exitflag, ~] = quadprog(H,f,A,b,Aeq,beq,lb,ub,[],options);
 if exitflag ~= 1
-    fprintf('[Warning] emp does not converge\n');
+    fprintf('[Warning] OCME does not converge\n');
 end
 
 %% Calculate the Variable
@@ -436,7 +444,7 @@ zmax = p_user(idx_zmax); var.zmax = zmax;
 
 %% Calculate the Four Kinds of Cost
 
-cost_pac = D*gamma_pac*(tin - Tin_ref)'*(tin - Tin_ref);
+cost_pac = D*gamma_pac*(tin - ones(24,1)*Tin_ref)'*(tin - ones(24,1)*Tin_ref);
 cost_pf = D*gamma_pf*(pf - Pf_ref)'*(pf - Pf_ref);
 cost_pc = D*gamma_pc*(pc - Pc_ref)'*(pc - Pc_ref);
 cost_po = D*gamma_po*(pi1*sum(z) + pi2*max(zmax));
@@ -445,6 +453,5 @@ cost_beta_W = gamma_po*c_W*beta_W;
 cost_beta_B = gamma_po*c_B*beta_B;
 
 cost = [cost_pac; cost_pf; cost_pc; cost_po; cost_beta_S; cost_beta_W; cost_beta_B];
-
 
 
