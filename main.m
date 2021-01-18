39caf0a5160d981f003affe480e4726256053b7b
Zeyu Yang <45043057+yangzeyu8@users.noreply.github.com>
2021-01-18 20:40:44 +0800

    Update main.m

--- main.m
+++ main.m
@@ -6,73 +6,20 @@
 close all; clear; format compact; rng(2342);
 
 %% Initialization
-
-% Read hourly usage data of 10 users
+%%% Read Hourly Usage Data of 10 Users
 hourly_usage_file = './data/hourly_usage_201609.csv';  % hourly usage
 hu_table = readtable(hourly_usage_file);
 hu = table2array(hu_table(:, 3));
-
-num_user = 1;
+%%% 10 Users & 10 Situations
+num_user = 10;
+num_situation = 10;
 
 %% Input Parameters
-
-% 1. param % parameters for optimization
-param.gamma_pac = 0.5; % discomfort coefficient for tin
-param.UBpac = 10; % upper boundary of pac
-param.LBpac = 0; % lower boundary of pac
-
-param.UBtin = 32; % upper boundary of tin
-param.LBtin = 15; % lower boundary of tin 
-
-param.gamma_pf = 0.3; % discomfort coefficient for pf
-param.UBpf = 7; % upper boundary flexible load per hour pf
-param.LBpf = 0; % lower boundary flexible load per hour pf
-
-param.gamma_pc = 0.4; % discomfort coefficient for pc
-param.UBpc = 7; % upper boundary curtailed load per hour pc
-param.LBpc = 0; % lower boundary curtailed load per hour pc
-
-param.UBpg = 20; % upper boundary grid load per hour pg
-param.LBpg = 0; % loweer boundary grid load per hour pg
-
-param.UBpch = 10; % upper boundary battery charge per capacity pch
-param.LBpch = 0; % lower boundary battery charge per capacity pch
-
-param.UBpdis = 10; % upper boundary battery discharge per capacity pdis
-param.LBpdis = 0; % lower boundary battery discharge per capacity pdis
-
-param.UBeb = 10; % upper boundary battery storage per capacity eb
-param.LBeb = 0; % lower boundary battery storage per capacity eb
-
-param.r_S = solar(); % solar power per capacity r_S, r_S is 24*1
-param.c_S = 5000; % solar cost per capacity c_S
-
-param.r_W = wind(); % wind power per capacity r_W, r_W is 24*1
-param.c_W = 2000; % wind cost per capacity c_W
-
-param.L_B = 100000; % battery life span L_B
-param.c_B = 3000;  % battery cost per capacity c_B
-
-param.gamma_po = 1; % discomfort coefficient for money
-
-param.prob = [0.2 0.01369863 0.043835616 0.120547945 0.024657534 0.046575342 0.249315068 0.208219178 0.068493151 0.024657534];
-
-% 2. Tin_ref % preferred temperature
-Tin_ref = 24;
-
-% 3. Tout % outdoor temperature of one day
-Tout = temperature(); % Read temperature data: 9/6~9/12 2016 at Austin
-
-% 4. Tin0 % indoor initial temperature
-Tin0 = 23*ones(num_user, 1);
-
-% 5. Pf_ref % preferred flexible load
-% 6. Pc_ref % preferred curtailed load
-% 7. Pil % inflexible load
-Pf_ref = zeros(24,10,7);
-Pc_ref = zeros(24,10,7);
-Pil = zeros(24,10,7);
-for n = 1:10
+%%% Initialization
+Pf_ref = zeros(24,num_user,7);
+Pc_ref = zeros(24,num_user,7);
+Pil = zeros(24,num_user,7);
+for n = 1:num_user
     for d = 1:7
         for t = 1:24
             Pil(t, n, d) = hu((n-1)*7*24 + (d-1)*24 + t);
@@ -91,27 +38,56 @@ for n = 1:10
     end
 end
 Pil = Pil - Pf_ref - Pc_ref;
-Pil = mean(mean(Pil,3),2);
-Pf_ref = mean(mean(Pf_ref,3),2);
-Pc_ref = mean(mean(Pc_ref,3),2);
-
-% 8. theta % renewable target percentage
-theta = 0.5;
-
-% 9. Eb0 % initial battery energy
+param.prob = [0.2 0.01369863 0.043835616 0.120547945 0.024657534 0.046575342 0.249315068 0.208219178 0.068493151 0.024657534];
+%%% A. Flexible Load (i, omega): param, Pf_ref
+param.gamma_pf = 0.3; 
+Pf_ref = mean(Pf_ref,3);
+%%% B. Curtailed Load (i, omega): param, Pc_ref
+param.gamma_pc = 0.4; 
+param.LBpc = 0; 
+param.UBpc = 7; 
+Pc_ref = mean(Pc_ref,3);
+%%% C. HVAC (i, omega): param, Tin_ref, Tout, Tin0
+param.gamma_pac = 0.5; 
+param.LBtin = 15; 
+param.UBtin = 32;
+param.C = 3.3;
+param.R = 1.35;
+param.eta_pac = 7;
+Tin_ref = 24;
+Tout = temperature(); % Read temperature data: 9/6~9/12 2016 at Austin
+Tin0 = 23;
+%%% D. Inflexible Load (Input Only): Pil
+Pil = mean(Pil,3);
+%%% E. Renewable Energy (omega): param
+param.r_S = solar(); % solar power per capacity
+param.r_S = param.r_S(:,1:num_situation);
+param.r_W = wind(); % wind power per capacity
+param.r_W = param.r_W(:,1:num_situation);
+%%% F. Power Grid (omega): param
+param.UBpg = 20*num_user; 
+param.LBpg = 0; 
+%%% G. Battery (omega): param, Eb0, D
+param.eta_ch = 0.9;
+param.eta_dis = 0.9; 
+param.eb_rate_max = 10;
+param.pch_rate_max = 10;
+param.pdis_rate_max = 10;
+param.eb_rate_life = 100000;
 Eb0 = 0.1;
-
-% 10. D % how many days do we observe
 D = 365*10;
-
-% 11. M % the investment budgets
-M = 5*10000;
+%%% H. Aggregate Supply (omega): Pil
+%%% I. Operator Cost (omega): param
+param.pi1 = 0.2; % 0.2
+param.pi2 = 0.8; % 0.8
+%%% J. Investment (operator): param, theta, Theta, M
+param.c_S = 1000;
+param.c_W = 1000;
+param.c_B = 1000;
+theta = 0.5;
+Theta = 0.1;
+M = 20*1000;
 
 %% total simulated 1 user
-
-[var, cost] = OCM(param, Tin_ref, Tout, Tin0, Pf_ref, Pc_ref, Pil, theta, Eb0, D, M);
-[varE, costE] = OCME(param, Tin_ref, Tout, Tin0, Pf_ref, Pc_ref, Pil, theta, Eb0, D, M);
-
+[var, cost, invest] = solver(num_user, num_situation, param, Pf_ref, Pc_ref, Tin_ref, Tout, Tin0, Pil, Eb0, D, theta, Theta, M);
 save 'output/results.mat'
-
-
